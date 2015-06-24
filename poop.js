b85f543f78d10ada668337615592bee7997b2538
Lancewiu <ranchwiu@gmail.com>
2015-06-24 19:34:17 -0400

    test call updated in poop.js

---
+++ poop.js
@@ -0,0 +1,10 @@
+/* What the fuck did you fucking say to me you fucking fagot.*/
+
+function getLocal(DeviceInfo[] devices){
+  if(devices.length > 1){
+    alert("Hey fuck you");
+  }
+}
+
+/*Test run of get local devices.  Fuck your mom.*/
+chrome.signedInDevices.get(true,getLocal);
